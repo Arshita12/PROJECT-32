733f8ec286e96289654570b7afdeafce0fcbd115
Arshita12 <74907362+Arshita12@users.noreply.github.com>
2021-03-16 14:54:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,159 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+
+var particle;
+var divisions = [];
+var particles = [particle];
+var plinkos = [];
+var line;
+
+var divisionHeight = 300;
+
+var gameState = "PLAY";
+
+var count = 0;
+var score = 0;
+
+function setup() {
+    createCanvas(800, 800);
+    engine = Engine.create();
+    world = engine.world;
+    ground = new Ground(width/2,height,width,20);
+  
+  
+     for (var k = 0; k <=width; k = k + 80) {
+       divisions.push(new Division(k, height-divisionHeight/2, 10, divisionHeight));
+     }
+  
+  
+      for (var j = 75; j <=width; j=j+50) 
+      {
+      
+         plinkos.push(new Plinko(j,75));
+      }
+  
+      for (var j = 50; j <=width-10; j=j+50) 
+      {
+      
+         plinkos.push(new Plinko(j,175));
+      }
+  
+       for (var j = 75; j <=width; j=j+50) 
+      {
+      
+         plinkos.push(new Plinko(j,275));
+      }
+  
+       for (var j = 50; j <=width-10; j=j+50) 
+      {
+      
+         plinkos.push(new Plinko(j,375));
+      }
+  
+      
+  
+      
+  }
+
+ 
+
+function draw() {
+    background("teal");
+    Engine.update(engine);
+    textSize(35)
+  text("Price : "+score,20,40);
+  fill("khaki");
+  
+ textSize(17)
+  text(" Godiva ", 5, 550);
+  textSize(15)
+  text("Hershey's ", 87, 550);
+  textSize(16)
+  text(" Lindit ", 167, 550);
+  textSize(15)
+  text(" Toblerone", 243, 550);
+  textSize(14)
+  text(" Dairy Milk ", 325, 550);
+  textSize(16)
+  text(" 5Star", 410, 550);
+  textSize(16)
+  text(" Mars ", 490, 550);
+  textSize(16)
+  text(" Twix ", 572, 550);
+  textSize(14)
+  text(" Sneakers", 645, 550);
+  textSize(16)
+  text("Guylian ", 729, 550);
+  
+
+
+
+    ground.display();
+    if ( gameState =="END") {
+        background("khaki");
+        fill("blue");
+        textSize(85);
+        text("CART IS FULL", 100, 200);
+       
+      } 
+      for(var k = 0; k < plinkos.length; k++) {
+       plinkos[k].display();
+    }
+
+    if(particle!=null)
+    {
+       particle.display();
+        
+        if (particle.body.position.y>700)
+        {
+              if (particle.body.position.x < 300) 
+              {
+                  score=score+500;      
+                  particle=null;
+                  if ( count>= 5) gameState ="END";                          
+              }
+
+
+              else if (particle.body.position.x < 600 && particle.body.position.x > 301 ) 
+              {
+                    score = score + 100;
+                    particle=null;
+                    if ( count>= 5) gameState ="END";
+
+              }
+              else if (particle.body.position.x < 900 && particle.body.position.x > 601 )
+              {
+                    score = score + 200;
+                    particle=null;
+                    if ( count>= 5)  gameState ="END";
+
+              }      
+              
+        }
+    }
+    
+
+   
+    
+  
+ for (var i = 0; i < divisions.length; i++) {
+     
+    divisions[i].display();
+  }
+  
+}
+
+
+function mousePressed() {
+    if(gameState !== "END") {
+        count++;
+    particle = new Particle(mouseX, 50, 10, 10);
+    }
+}
+
+
+
+
